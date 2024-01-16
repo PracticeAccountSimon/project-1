100df556ed27bb36524b10388bb51e401288214c
PracticeAccountSimon <simontraub@g.ucla.edu>
2024-01-15 19:37:12 -0800

    Removed confidential information

--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
-# Useless comment is not here
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.32.1"
+    }
+  }
 
-provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIAZI2LFRIYI7GZK36H"
-    secret_key = "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"
+  required_version = ">= 1.2.0"
 }
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-032346ab877c418af"
-  instance_type = "t3.micro"
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06aa3f7caf3a30282"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "ExampleAppServerInstance"
   }
 }
